0830380202b02eb0e86da67ca963d1420eb8b2da
donsalari <ameyakoshti@gmail.com>
2016-11-28 13:48:33 -0800

    linting

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -49,7 +49,14 @@ const About = () => (
       <Header image="about" />
 
       <h1 style={styles.titleHeader}> About </h1>
-      <p style={styles.subText}> Progressive Beer is a web app that is designed to leverage all the great features of Service Workers and build a truly Progressive Web App. If you'd like to learn more about Progressive Web Apps, I recommend checking out <p style={styles.link}><a href="https://developers.google.com/web/progressive-web-apps/">this link</a></p> for more information. Oh, and once you've visited a web page on this site, it will be available offline! </p>
+      <p style={styles.subText}> Progressive Beer is a web app that is designed to leverage
+        all the great features of Service Workers and build a truly Progressive Web App.
+        If you'd like to learn more about Progressive Web Apps, I recommend checking out
+        <span style={styles.link}>
+         <a href="https://developers.google.com/web/progressive-web-apps/"> this link</a>
+        </span> for more information. Oh, and once you've visited a web page on this site,
+        it will be available offline!
+      </p>
 
       <Card>
         <CardMedia
@@ -61,8 +68,12 @@ const About = () => (
       </Card>
 
       <h1 style={styles.dataHeader}> Our Data </h1>
-      <p style={styles.link}><img style={styles.breweryDb} src={`/js/${brewerydbImage}`} /></p>
-      <p style={styles.subText}> All of our data is supplied by the team over at BreweryDb.com. If you'd like to learn more about them, please check out their website for a huge database of breweries, beers, beer events and even brewers guilds! You can even submit new beers as the database is constantly kept up to date. </p>
+      <p style={styles.link}><img style={styles.breweryDb} src={brewerydbImage} /></p>
+      <p style={styles.subText}> All of our data is supplied by the team over at BreweryDb.com.
+        If you'd like to learn more about them, please check out their website for a huge
+        database of breweries, beers, beer events and even brewers guilds! You can even submit
+        new beers as the database is constantly kept up to date.
+      </p>
 
       <Footer />
     </div>
