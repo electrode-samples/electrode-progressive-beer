e1303e0e162c4ce631d8f3327532dec628437246
donsalari <ameyakoshti@gmail.com>
2016-12-12 16:23:34 -0800

    removing push notifications

--- client/sw-events.js
+++ client/sw-events.js
@@ -1,19 +1,1 @@
-/* eslint-env serviceworker */
-
 // This file contains all the user defined service worker related events
-
-import icon from "./images/beer-icon-192.png";
-import badge from "./images/beer-icon-96.png";
-
-self.addEventListener("push", (event) => {
-  const title = "It worked!";
-  const options = {
-    body: "Great job sending that push notification!",
-    tag: "electrode-push-notification-test",
-    icon,
-    badge
-  };
-  event.waitUntil(
-    self.registration.showNotification(title, options)
-  );
-});
