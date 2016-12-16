529be1e22ca2697a5531973ba0693c300d9f583e
donsalari <ameyakoshti@gmail.com>
2016-12-16 14:03:51 -0800

    removing comments

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,6 @@
 import "isomorphic-fetch";
 import React from "react";
 import {render} from "react-dom";
-// import Helmet from "react-helmet";
 import {Router, browserHistory} from "react-router";
 import {createStore, compose, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
@@ -16,8 +15,6 @@ require.ensure(["./sw-registration"], (require) => {
 }, "sw-registration");
 
 const enhancer = compose(
-  // Add middlewares you want to use in development:
-  // applyMiddleware(d1, d2, d3),
   applyMiddleware(thunk),
   DevTools.instrument()
 );
@@ -28,13 +25,6 @@ window.webappStart = () => {
 
   render(
       <Provider store={store}>
-        {/* <Helmet
-          link={[
-            {"rel": "stylesheet", "href": "https://fonts.googleapis.com/icon?family=Material+Icons"},
-            {"rel": "canonical", "href": "http://mysite.com/example"}
-          ]}
-          script={[{"src": "http://include.com/pathtojs.js", "type": "text/javascript"}]}
-        /> */}
         <div>
           <Router history={browserHistory}>{routes}</Router>
           <DevTools />
